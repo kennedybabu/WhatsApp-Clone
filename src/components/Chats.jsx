2cc7cfe531b74d380973d6eaff67dade39eb6f74
kennedybabu <kennedynjogu8@gmail.com>
2022-12-03 21:48:55 +0300

    feature: style and move the chats component

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import {FiChevronDown} from "react-icons/fi";
+
 
 const Chats = () => {
-  return (
-    <div>Chats</div>
+  return (    
+     <div className='w-full'>
+     <div className='w-full h-[72px] flex items-center px-[14px] cursor-pointer hover:bg-[#f0eeec] transition'>
+         <div className='w-[49px] h-[49px] bg-red-300 rounded-full'>
+
+         </div>
+         <div className='w-full h-full flex flex-col items-center justify-center border-bottom ml-[15px]'>
+             <div className='w-full flex items-center justify-between'>
+               <p className='text-[#111b21] font-[17px]'>Ritaj</p>
+               <small className='font-[12px] text-[#667781]'>9:02pm</small>
+             </div>
+             <div className='w-full relative text-[#667781] group'>
+                 <p className='font-[14px]'>I'll definitely forward it</p>
+                 <FiChevronDown className='top-0 right-0 font-bold hidden group-hover:absolute'/>
+             </div>
+         </div>
+     </div>
+   </div>
   )
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Search from "./Search"
+import Chats from './Chats';
 import {BiArchiveIn} from "react-icons/bi";
+
+
 
 const Sidebar = () => {
   return (
@@ -9,13 +12,16 @@ const Sidebar = () => {
       <Navbar />
       <Search />
       <div className='w-full h-[49px] flex items-center cursor-pointer'>
-        <div className='px-[14px] w-[49px]'>
+        <div className='ml-[14px]  px-[14px] w-[49px]'>
           <BiArchiveIn className='text-[#00a884] text-[18px]'/>
         </div>
-        <div className='border-bottom w-full h-full flex items-center'>
-          <p className='text-[17px]'>Archived</p>
+        <div className='border-bottom ml-[15px] w-full h-full flex items-center'>
+          <p className='text-[17px]'>Archived</p>          
         </div>
       </div>
+
+      {/* chats */}
+      <Chats />
     </div>
   )
 }
