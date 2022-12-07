0a87f2981830b22c3897a4b025a2a7baebc58278
kennedybabu <kennedynjogu8@gmail.com>
2022-12-07 10:48:44 +0300

    ft:search and select users to view or start a chat

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,31 +1,50 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import {FiChevronDown} from "react-icons/fi";
 import {BiCheckDouble} from "react-icons/bi";
+import { doc, onSnapshot } from 'firebase/firestore';
+import { db } from '../Firebase';
+import { AuthContext } from '../context/AuthContext';
 
 
 const Chats = () => {
   const value = [1,2,3,4,5,6,7,8,9,10]
-  return (    
-     <div className='w-full h-[530px]'>
-      {value.map((chat,index) => {
-        return (
-        <div key={index} className='bg-[#ffffff] chat w-full group  h-[72px] flex items-center px-[14px] cursor-pointer hover:bg-[#f0eeec] transition'>
-            <div className='w-[49px] h-[49px] bg-red-300 rounded-full px-6'>
+  const [chats, setChats] = useState([])
 
+  const {currentUser} = useContext(AuthContext)
+
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data())
+    });
+      return () => {
+        unsub()
+      }
+    }
+    currentUser.uid && getChats()  
+  }, [currentUser.uid])
+
+
+  return (    
+  <div className='w-full h-[530px]'>
+      {Object.entries(chats)?.map(chat => (
+         <div key={chat[0]} className='bg-[#ffffff] chat w-full group  h-[72px] flex items-center px-[14px] cursor-pointer hover:bg-[#f0eeec] transition'>
+          <div className='w-[49px] h-[49px] rounded-full px-6'>
+              <img src={chat[1].userInfo.photoURL} alt="/" className='w-full h-full'/>
+          </div>
+          <div className='w-full h-full flex flex-col items-center justify-center border-bottom ml-[15px]'>
+            <div className='w-full flex items-center justify-between'>
+              <p className='text-[#111b21] font-[17px]'>{chat[1].userInfo.displayName}</p>
+              <small className='font-[12px] text-[#667781]'>9:02pm</small>
             </div>
-            <div className='w-full h-full flex flex-col items-center justify-center border-bottom ml-[15px]'>
-                <div className='w-full flex items-center justify-between'>
-                  <p className='text-[#111b21] font-[17px]'>Ritaj</p>
-                  <small className='font-[12px] text-[#667781]'>9:02pm</small>
-                </div>
-                <div className='w-full relative text-[#667781]'>
-                    <p className='font-[14px] flex items-center'><BiCheckDouble className='mr-1'/>I'll definitely forward it</p>
-                    <FiChevronDown className='absolute top-0 right-0 font-bold hidden group-hover:block'/>
-                </div>
+            <div className='w-full relative text-[#667781]'>
+                <p className='font-[14px] flex items-center'><BiCheckDouble className='mr-1'/>{chat[1].userInfo.lastMessage?.text}</p>
+                <FiChevronDown className='absolute top-0 right-0 font-bold hidden group-hover:block'/>
             </div>
-          </div> 
-        )
-      })}         
+          </div>
+       </div> 
+      ))}
    </div>
   )
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import {AiOutlineSearch} from "react-icons/ai";
 import {BsFilter} from "react-icons/bs"
 import {FiChevronDown} from "react-icons/fi";
 import {BiCheckDouble} from "react-icons/bi"
 
 import { db } from '../Firebase';
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, query, where, getDocs, getDoc, setDoc, doc, updateDoc, serverTimestamp } from "firebase/firestore";
+import { AuthContext } from '../context/AuthContext';
 
 
 
@@ -14,6 +15,8 @@ const Search = () => {
   const [username, setUsername] = useState('')
   const [user, setUser] = useState(null)
   const [error, setError] = useState('')
+
+  const {currentUser} = useContext(AuthContext)
 
   const handleSearch = async (e) => {
     const q = query(collection(db, "users"), where("displayName", "==", username))
@@ -32,6 +35,43 @@ const Search = () => {
     e.code === "Enter" && handleSearch()
   }
 
+  const handleSelect = async () => {
+    //check whether the group(chats in firestore) exists
+    const combinedId = currentUser.uid > user.uid ? currentUser.uid + user.uid : user.uid + currentUser.uid
+    try {
+      const response = await getDoc(doc(db, "chats", combinedId))
+
+      if(!response.exists()) {
+        await setDoc(doc(db, "chats", combinedId),{messages:[]})
+
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedId+".userInfo"]: {
+            uid:user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL
+          },
+          [combinedId+".date"]: serverTimestamp()
+        })
+
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [combinedId+".userInfo"]: {
+            uid:currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL
+          },
+          [combinedId+".date"]: serverTimestamp()
+        })
+
+      }
+
+
+    } catch(error) {
+      setError(error)
+    }
+    setUser(null)
+    setUser('')
+  }
+
 
  
 
@@ -40,7 +80,7 @@ const Search = () => {
       <div className='flex  pl-[12px] h-[49px] items-center justify-between'>
         <div  className="bg-[#f0f2f5] rounded-[8px] h-[35px] flex items-center pr-[32px] w-[412px]">
           <AiOutlineSearch className='ml-4'/>
-          <input onChange={(e) => setUsername(e.target.value)} onKeyDown={handleKey} className="bg-[#f0f2f5] ml-[65px] outline-none border-none" type="text" placeholder='search or start new chat' />
+          <input value={username} onChange={(e) => setUsername(e.target.value)} onKeyDown={handleKey} className="bg-[#f0f2f5] ml-[65px] outline-none border-none" type="text" placeholder='search or start new chat' />
         </div>
         <BsFilter className='text-[20px] mr-2'/>
       </div>
@@ -48,8 +88,8 @@ const Search = () => {
 
       {/* render searched user */}
      { user && 
-      <div className='bg-[#ffffff] chat w-full group  h-[72px] flex items-center px-[14px] cursor-pointer hover:bg-[#f0eeec] transition'>
-            <div className='w-[49px] h-[49px] bg-red-300 rounded-full px-6'>
+      <div onClick={handleSelect} className='bg-[#ffffff] chat w-full group  h-[72px] flex items-center px-[14px] cursor-pointer hover:bg-[#f0eeec] transition'>
+            <div className='w-[49px] h-[49px] rounded-full px-6'>
                 <img src={user.photoURL} alt="/" />
             </div>
             <div className='w-full h-full flex flex-col items-center justify-center border-bottom ml-[15px]'>
