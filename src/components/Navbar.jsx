4986beaed10ef3d5e34ff6d1b6efaa7b263ce3d5
kennedybabu <kennedynjogu8@gmail.com>
2022-12-07 09:10:09 +0300

    feature: render the users details

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {AuthContext} from "../context/AuthContext"
 import {HiUserGroup} from "react-icons/hi";
 import {BsFillChatLeftTextFill} from "react-icons/bs";
 import {TbCircleDashed} from "react-icons/tb";
 import {IoMdMore} from "react-icons/io";
 
 const Navbar = () => {
+  const {currentUser} = useContext(AuthContext)
+
+
   return (
     <div className='h-[59px] bg-[#f0f2f5] w-full flex items-center justify-between px-[16px] py-[10px]'>
       <div>
-        <div className='w-[40px] h-[40px] bg-red-300 rounded-full'>
-
+        <div className='w-[40px] h-[40px] rounded-full'>
+          <img src={currentUser.photoURL} alt="/" className='w-full h-full rounded-full' />
         </div>
       </div>
       <div className='flex items-center text-[#54656f] w-[190px] justify-between'>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
             <input className='my-4 p-4 border' type="email" name="email" id="email" placeholder='email' />
             <input className='my-4 p-4' type="password" name="password" id="password" placeholder='password' />
             
-            <button className='uppercase ft-btn my-[16px] mx-auto px-[32px] py-[16px] border rounded-[40px] text-[#77d7c8]'>Sign Up</button>
+            <button className='uppercase ft-btn my-[16px] mx-auto px-[32px] py-[16px] border rounded-[40px] text-[#77d7c8]'>login</button>
             <div className='w-full flex items-center justify-between text mt-[24px]'>
               <small>Don't have an account ?</small>
               <Link to="/register">Sign Up</Link>
